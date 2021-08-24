fdff09f16d4fbbbfd0378c4d6bf0d0a623efb05b
onggiabayluon <livipst2@gmail.com>
2021-08-24 15:28:53 +0700

    - edit domain name

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(comicweb.test www.comicweb.test)
+domains=(cloudimagewall.xyz www.cloudimagewall.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="livipst2@gmail.com" # Adding a valid address is strongly recommended
