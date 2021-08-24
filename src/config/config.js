18917117dc53e41da84edcc49cb3582de88ce369
onggiabayluon <livipst2@gmail.com>
2021-08-24 14:57:08 +0700

    - nginx inprogress

--- src/config/config.js
+++ src/config/config.js
@@ -14,28 +14,12 @@ module.exports = {
     // WASABI_REGION: process.env.WASABI_REGION || "ap-northeast-1",
     // IMAGE_URL: process.env.URL || "https://s3.ap-northeast-1.wasabisys.com/cloudimagewallbucket",
 
-    MONGO_IP: process.env.MONGO_IP || "mongo",
-    MONGO_PORT: process.env.MONGO_PORT || 27017,
-    MONGO_USER: process.env.MONGO_USER || 'ducchuy',
-    MONGO_PASSWORD: process.env.MONGO_PASSWORD || 'mypassword',
-    MONGO_FOLDER: process.env.MONGO_FOLDER || "mydb",
-    REDIS_URL: process.env.REDIS_URL || "localhost",
-    REDIS_PORT: process.env.REDIS_PORT || 6379,
-
-    WASABI_ACCESS_KEY_ID: process.env.WASABI_ACCESS_KEY_ID || '9FP3U7TYG1CK3QRBI1NC',
-    WASABI_SECRET_ACCESS_KEY: process.env.WASABI_SECRET_ACCESS_KEY || 'HehGeUmLyGiMpbRzK1OyDp9IHtvr0eG2XX3N0O1h',
-    WASABI_ENDPOINT: process.env.WASABI_ENDPOINT || 's3.ap-northeast-1.wasabisys.com',
-    WASABI_BUCKET_NAME: process.env.WASABI_BUCKET_NAME || "cloudimagewallbucket",
-    WASABI_REGION: process.env.WASABI_REGION || "ap-northeast-1",
-    IMAGE_URL: process.env.URL || "https://s3.ap-northeast-1.wasabisys.com/cloudimagewallbucket",
-    
-    // DEV testing ONLY
     // MONGO_IP: process.env.MONGO_IP || "mongo",
     // MONGO_PORT: process.env.MONGO_PORT || 27017,
     // MONGO_USER: process.env.MONGO_USER || 'ducchuy',
     // MONGO_PASSWORD: process.env.MONGO_PASSWORD || 'mypassword',
     // MONGO_FOLDER: process.env.MONGO_FOLDER || "mydb",
-    // REDIS_URL: process.env.REDIS_URL || "redis",
+    // REDIS_URL: process.env.REDIS_URL || "localhost",
     // REDIS_PORT: process.env.REDIS_PORT || 6379,
 
     // WASABI_ACCESS_KEY_ID: process.env.WASABI_ACCESS_KEY_ID || '9FP3U7TYG1CK3QRBI1NC',
@@ -44,6 +28,22 @@ module.exports = {
     // WASABI_BUCKET_NAME: process.env.WASABI_BUCKET_NAME || "cloudimagewallbucket",
     // WASABI_REGION: process.env.WASABI_REGION || "ap-northeast-1",
     // IMAGE_URL: process.env.URL || "https://s3.ap-northeast-1.wasabisys.com/cloudimagewallbucket",
+    
+    // DEV testing ONLY
+    MONGO_IP: process.env.MONGO_IP || "mongo",
+    MONGO_PORT: process.env.MONGO_PORT || 27017,
+    MONGO_USER: process.env.MONGO_USER || 'ducchuy',
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD || 'mypassword',
+    MONGO_FOLDER: process.env.MONGO_FOLDER || "mydb",
+    REDIS_URL: process.env.REDIS_URL || "redis",
+    REDIS_PORT: process.env.REDIS_PORT || 6379,
+
+    WASABI_ACCESS_KEY_ID: process.env.WASABI_ACCESS_KEY_ID || '9FP3U7TYG1CK3QRBI1NC',
+    WASABI_SECRET_ACCESS_KEY: process.env.WASABI_SECRET_ACCESS_KEY || 'HehGeUmLyGiMpbRzK1OyDp9IHtvr0eG2XX3N0O1h',
+    WASABI_ENDPOINT: process.env.WASABI_ENDPOINT || 's3.ap-northeast-1.wasabisys.com',
+    WASABI_BUCKET_NAME: process.env.WASABI_BUCKET_NAME || "cloudimagewallbucket",
+    WASABI_REGION: process.env.WASABI_REGION || "ap-northeast-1",
+    IMAGE_URL: process.env.URL || "https://s3.ap-northeast-1.wasabisys.com/cloudimagewallbucket",
 
     
     UPDATE_PER_MIN: 10,
