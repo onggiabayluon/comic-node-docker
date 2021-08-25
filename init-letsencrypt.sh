ac0f3ff0a286722484e34ac4c081b20f916ddfac
onggiabayluon <livipst2@gmail.com>
2021-08-25 08:08:44 +0700

    small change

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(cloudimagewall.xyz www.cloudimagewall.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="livipst2@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -39,7 +39,7 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate --no-deps -d nginx
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate --no-deps -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
