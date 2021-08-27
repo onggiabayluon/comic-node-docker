5af583423e7ec4881ce73c41da97a1fd0e4843b6
onggiabayluon <livipst2@gmail.com>
2021-08-27 19:21:35 +0700

    minor

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,7 @@ const passport = require('passport');
 const { User } = require('../app/models/User');
 // Load Controller
 const UserController = require('../app/controllers/UserController');
-const Sitecontroller = require('../app/controllers/Sitecontroller');
+const SiteController = require('../app/controllers/SiteController');
 // Load Middleware
 const { forwardAuthenticated } = require('../config/auth/auth');
 
@@ -30,10 +30,10 @@ router.delete('/deleteUser/:userId', UserController.deleteUser);
 
 // Facebook 
 router.get('/facebook/login', passport.authenticate('facebook', { scope: ['email'] }));
-router.get('/facebook/callback',passport.authenticate('facebook'), Sitecontroller.index);
+router.get('/facebook/callback',passport.authenticate('facebook'), SiteController.index);
 
 // Google 
 router.get('/google/login', passport.authenticate('google', { scope: ['profile'] }));
-router.get('/google/callback',passport.authenticate('google'), Sitecontroller.index);
+router.get('/google/callback',passport.authenticate('google'), SiteController.index);
 
 module.exports = router;
